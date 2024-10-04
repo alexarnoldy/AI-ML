cb218ee4411cabaf647dfb07f0d0428ac41b51a1
Alex Arnoldy <alexarnoldy@gmail.com>
2024-10-04 16:25:47 -0700

    Delete SUSE-Private-AI/set-up-cert-manager/02-install-cert-manager.cmd

--- SUSE-Private-AI/set-up-cert-manager/02-install-cert-manager.cmd
+++
@@ -1,3 +0,0 @@
-helm repo add jetstack https://charts.jetstack.io
-helm repo update
-helm install   cert-manager jetstack/cert-manager   --namespace cert-manager   --create-namespace   --version v1.15.2     --set crds.enabled=true
