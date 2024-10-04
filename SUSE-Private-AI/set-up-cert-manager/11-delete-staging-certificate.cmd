9b505be43d0f37e25a395946d6470db1ca049543
Alex Arnoldy <alexarnoldy@gmail.com>
2024-10-04 16:34:07 -0700

    Update 11-delete-staging-certificate.cmd

--- SUSE-Private-AI/set-up-cert-manager/11-delete-staging-certificate.cmd
+++ SUSE-Private-AI/set-up-cert-manager/11-delete-staging-certificate.cmd
@@ -1,3 +1,3 @@
 source /tmp/variables && \
-kubectl -n ${APPLICATION_NAMESPACE} delete secret ${APPLICATION_NAME}-staging-certificate-secret
+kubectl -n ${APPLICATION_NAMESPACE} delete secret ${APPLICATION_NAME}-tls
 kubectl -n ${APPLICATION_NAMESPACE} delete certificate ${APPLICATION_NAME}-staging-certificate
